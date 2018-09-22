5b41a1505692f970a6ce4eae7903e89a28488837
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-09-22 15:00:01 -0500

    add lab 3 and exercise 1

---
+++ Lab3/LifeCycleExperiments/app/src/main/java/com/example/jake/lifecycleexperiments/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.jake.lifecycleexperiments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String createMsg;
+    private String startMsg;
+    private String resumeMsg;
+    private String pauseMsg;
+    private String stopMsg;
+    private String restartMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initMessages();
+
+        showToast(createMsg);
+    }
+
+    private void initMessages(){
+        createMsg = getResources().getText(R.string.create_message).toString();
+        startMsg = getResources().getText(R.string.start_message).toString();
+        resumeMsg = getResources().getText(R.string.resume_message).toString();
+        pauseMsg = getResources().getText(R.string.pause_message).toString();
+        stopMsg = getResources().getText(R.string.stop_message).toString();
+        restartMsg = getResources().getText(R.string.restart_message).toString();
+    }
+
+    //added this method to make my life easier
+    private void showToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show();
+        Log.d("msg: ", msg);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        showToast(startMsg);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        showToast(resumeMsg);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        showToast(pauseMsg);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        showToast(stopMsg);
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        showToast(restartMsg);
+    }
+}
